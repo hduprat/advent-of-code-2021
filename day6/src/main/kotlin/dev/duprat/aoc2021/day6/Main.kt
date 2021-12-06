9a5024a6021787c4ec99a488b93946c1fbbcac54
hduprat <hugod@bam.tech>
2021-12-06 12:51:30 +0100

    :lantern: :fish: Finish Day 6

---
+++ day6/src/main/kotlin/dev/duprat/aoc2021/day6/Main.kt
@@ -0,0 +1,13 @@
+package dev.duprat.aoc2021.day6
+
+class Main {}
+
+fun main() {
+  println("Exercice 1")
+  val exercise1 = Day6ScenarioVariant(80)
+  println(exercise1.countLanternFishes())
+
+  println("Exercice 2")
+  val exercise2 = Day6ScenarioVariant(256)
+  println(exercise2.countLanternFishes())
+}
