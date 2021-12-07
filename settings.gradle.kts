2dccf618148eec789eaf98c45b3059e732a2c8fe
hduprat <hugod@bam.tech>
2021-12-07 09:28:06 +0100

    :crab: Finish Day 7

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,4 +9,4 @@
  */
 
 rootProject.name = "advent-of-code-2021"
-include("app", "day1", "day2", "day3", "day4", "day5", "day6", "utils", "list")
+include("app", "day1", "day2", "day3", "day4", "day5", "day6", "day7", "utils", "list")
