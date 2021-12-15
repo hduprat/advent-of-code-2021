4736f6814565dce5a9dccef9d97523c7a2c871cb
hduprat <hugod@bam.tech>
2021-12-15 20:11:59 +0100

    :shell: Finish Day 15

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,4 +9,5 @@
  */
 
 rootProject.name = "advent-of-code-2021"
-include("app", "day1", "day2", "day3", "day4", "day5", "day6", "day7", "day8", "day9", "day10", "day11", "day12", "day13", "day14", "utils", "list")
+include("app", "day1", "day2", "day3", "day4", "day5", "day6", "day7", "day8", "day9", "day10", "day11", "day12",
+"day13", "day14", "day15", "utils")
